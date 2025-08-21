bc543b98cfc0e17a1ee3d46679e43ba9dd0e56ef
Chochain Lee <chochain@yahoo.com>
2025-08-21 09:38:20 +0800

    ~/src/eforth/FV.kt make FV class open (for Dict)

--- src/eforth/FV.kt
+++ src/eforth/FV.kt
@@ -4,7 +4,7 @@
 ///
 package eforth
 
-class FV<T> : ArrayList<T>() {                 ///< eForth ArrayList (i.e. vector)
+open class FV<T> : ArrayList<T>() {         ///< eForth ArrayList (i.e. vector)
     fun head(): T = get(0)
     fun tail(): T = get(size - 1)
     fun tail(offset: Int): T = get(size - offset)
@@ -13,7 +13,6 @@ class FV<T> : ArrayList<T>() {                 ///< eForth ArrayList (i.e. vecto
         addAll(lst)
         return this
     }
-    
     fun drop(): FV<T> {
         removeAt(size - 1)
         return this
