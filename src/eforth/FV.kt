2f8a4acf372de4db12920dac4950af4c2ec0ea86
Chochain Lee <chochain@yahoo.com>
2025-08-22 19:32:49 +0800

    ~/src/eforth/FV.kt no need merge, drop (+=, dropLast)

--- src/eforth/FV.kt
+++ src/eforth/FV.kt
@@ -5,16 +5,6 @@
 package eforth
 
 open class FV<T> : ArrayList<T>() {         ///< eForth ArrayList (i.e. vector)
-    fun head(): T = get(0)
     fun tail(): T = get(size - 1)
     fun tail(offset: Int): T = get(size - offset)
-    
-    fun merge(lst: FV<T>): FV<T> {
-        addAll(lst)
-        return this
-    }
-    fun drop(): FV<T> {
-        removeAt(size - 1)
-        return this
-    }
 }
