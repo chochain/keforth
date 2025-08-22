4ece6a41b3bd7e332df35994359a9dc85ed4c9ea
Chochain Lee <chochain@yahoo.com>
2025-08-22 17:05:25 +0800

    ~/src/eforth/Cocde.kt create Unnest alias; use Xt, DU aliases (in VM.kt)

--- src/eforth/Code.kt
+++ src/eforth/Code.kt
@@ -6,8 +6,10 @@ package eforth
 
 import java.util.*
 
+typealias Unnest = ArithmeticException
+
 class Immd : Code {
-    constructor(n: String, f: (Code) -> Unit) : super(n, true, f) {}  ///< immediate words
+    constructor(n: String, f: Xt) : super(n, true, f) {}  ///< immediate words
 }
 open class Code {
     companion object {
@@ -19,28 +21,28 @@ open class Code {
     var token: Int = 0
     var stage: Int = 0
     
-    var xt: ((Code) -> Unit)? = null       ///< execution token
+    var xt: Xt? = null                     ///< execution token
     var pf = FV<Code>()                    ///< if..pf..
     var p1 = FV<Code>()                    ///< else..p1..then
     var p2 = FV<Code>()                    ///< aft..next
-    var qf = FV<Int>()                     ///< variable storage
+    var qf = FV<DU>()                      ///< variable storage
     var str: String? = null                ///< string storage
     ///
     ///> constructors
     ///
-    constructor(n: String, im: Boolean=false, f: (Code) -> Unit) {  ///< built-in words
+    constructor(n: String, im: Boolean=false, f: Xt) {  ///< built-in words
         name = n; xt = f; immd = im; token = fence++
     }
-    constructor(n: String) {                                  ///< colon words
+    constructor(n: String) {                      ///< colon words
         name = n; token = fence++
     }
-    constructor(f: (Code) -> Unit, n: String) {               ///< branching nodes
+    constructor(f: Xt, n: String) {               ///< branching nodes
         name = n; xt = f
     }
-    constructor(f: (Code) -> Unit, n: String, d: Int) {       ///< int literal
+    constructor(f: Xt, n: String, d: DU) {        ///< int literal
         name = n; xt = f; qf.add(d)
     }
-    constructor(f: (Code) -> Unit, n: String, s: String) {    ///< string literal
+    constructor(f: Xt, n: String, s: String) {    ///< string literal
         name = n; xt = f; str = s
     }
     ///
@@ -50,9 +52,9 @@ open class Code {
     ///
     ///> variable storage management methods
     ///
-    fun comma(v: Int)          = pf.head().qf.add(v)
-    fun setVar(i: Int, v: Int) = pf.head().qf.set(i, v)
-    fun getVar(i: Int): Int    = pf.head().qf.get(i)
+    fun comma(v: DU)          = pf.head().qf.add(v)
+    fun setVar(i: Int, v: DU) = pf.head().qf.set(i, v)
+    fun getVar(i: Int): DU    = pf.head().qf.get(i)
     ///
     ///> inner interpreter
     ///
@@ -60,20 +62,20 @@ open class Code {
         xt?.let { it(this); return }
         for (w in pf) {
             try { w.nest() }
-            catch (e: ArithmeticException) { break; } /// * capture UNNEST
+            catch (e: Unnest) { break; }           /// * capture UNNEST
         }
     }
     fun nest(pf: FV<Code>) { for (w in pf) { w.nest() } }
-    fun unnest() { throw ArithmeticException() }
+    fun unnest() { throw Unnest() }
     ///
     ///> branching, looping methods
     ///
-    fun branch(ss: Stack<Int>) {
+    fun branch(ss: Stack<DU>) {
         for (w in if (ss.pop() != 0) pf else p1) {
             w.nest()
         }
     }
-    fun begin(ss: Stack<Int>) {
+    fun begin(ss: Stack<DU>) {
         val b = stage
         while (true) {
             nest(pf)                              /// * begin..
@@ -83,9 +85,9 @@ open class Code {
             nest(p1)
         }
     }
-    fun dofor(rs: Stack<Int>) {
+    fun dofor(rs: Stack<DU>) {
         try {
-            var i: Int
+            var i: DU
             val b = stage
             do {
                 nest(pf)                          ///> for..
@@ -101,13 +103,13 @@ open class Code {
                 if (i < 0) break
                 nest(p1)
             }
-        } catch (e: Exception) {
+        } catch (e: Unnest) {                     /// * handle EXIT
             /* leave */
         } finally { rs.pop() }                    ///> pop off index
     }
-    fun loop(rs: Stack<Int>) {                    ///> do..loop
+    fun loop(rs: Stack<DU>) {                     ///> do..loop
         try {
-            var i: Int
+            var i: DU
             val m = rs.pop()
             while (true) {
                 nest(pf)
@@ -115,7 +117,7 @@ open class Code {
                 rs.push(++i)
                 if (i >= m) break
             }
-        } catch (e: ArithmeticException) {        /// * handle LEAVE
+        } catch (e: Unnest) {                      /// * handle LEAVE
             /* leave */
         } finally { rs.pop() }
     }
