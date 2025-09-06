32b3eb1382b81a4e45b20b370875d1cb311ed2ba
Chochain Lee <chochain@yahoo.com>
2025-09-06 11:27:26 +0800

    ~/java/com.demo/Elogo.java show=>debug,log (OutputHandler)

--- app/src/main/java/com.demo/Elogo.java
+++ app/src/main/java/com.demo/Elogo.java
@@ -5,17 +5,17 @@ package com.demo.logo;
 import com.demo.ui.OutputHandler;
 
 public class Elogo {
-    public static void process(
-        String msg, Logo1 logo, OutputHandler out) {
-        String[] ops = msg.split("\\s+(?=(?:[^']*'[^']*')*[^']*$)");  ///< parse command
+    public static void process(String msg, Logo1 logo, OutputHandler out) {
+        final String rx = "\\s+(?=(?:[^']*'[^']*')*[^']*$)"; ///< regex single quotes
+        String[] ops = msg.split(rx);                        ///< parse parameters
         int      n   = ops.length;
         
         // Debug output
-        out.showCallback(msg + " n=" + n);
+        out.log(msg + " n=" + n);
         for (int i = 0; i < n; i++) {
-            out.showCallback(" " + i + ":" + ops[i]);
+            out.log(" " + i + ":" + ops[i]);
         }
-        out.showCallback("\n");
+        out.log("\n");
         
         if (n < 1) return;
 
@@ -23,8 +23,8 @@ public class Elogo {
         String v1 = n > 1 ? ops[1] : "0";
         String v2 = n > 2 ? ops[2] : "0";
             
-        out.showCallback("before " + logo.to_s() + "\n");
+        out.debug("before " + logo.to_s() + "\n");
         logo.update(op, v1, v2);
-        out.showCallback("after  " + logo.to_s() + "\n");
+        out.debug("after  " + logo.to_s() + "\n");
     }
 }
