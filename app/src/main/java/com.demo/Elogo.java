6913805550876a218006d6cd7c8ce7ec2b595657
Chochain Lee <chochain@yahoo.com>
2025-09-06 19:30:42 +0800

    ~/java/com.demo/Elogo.java borrow Runnable for optional trace

--- app/src/main/java/com.demo/Elogo.java
+++ app/src/main/java/com.demo/Elogo.java
@@ -9,22 +9,23 @@ public class Elogo {
         final String rx = "\\s+(?=(?:[^']*'[^']*')*[^']*$)"; ///< regex single quotes
         String[] ops = msg.split(rx);                        ///< parse parameters
         int      n   = ops.length;
-        
-        // Debug output
-        out.log(msg + " n=" + n);
-        for (int i = 0; i < n; i++) {
-            out.log(" " + i + ":" + ops[i]);
-        }
-        out.log("\n");
-        
+
+        Runnable trace = () -> {                             ///< borrowed interface
+            out.log(msg + " n=" + n);                        ///  to trace params
+            for (int i = 0; i < n; i++) {
+                out.log(" " + i + ":" + ops[i]);
+            }
+            out.log("\n");
+        };
+        // trace.run();
         if (n < 1) return;
 
         String op = ops[0];
         String v1 = n > 1 ? ops[1] : "0";
         String v2 = n > 2 ? ops[2] : "0";
-            
-        out.debug("before " + logo.to_s() + "\n");
+
+        // out.debug("before " + logo.to_s() + "\n");
         logo.update(op, v1, v2);
-        out.debug("after  " + logo.to_s() + "\n");
+        // out.debug("after  " + logo.to_s() + "\n");
     }
 }
