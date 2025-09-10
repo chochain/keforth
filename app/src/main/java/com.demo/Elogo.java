015237015c489799b6cbb8e4a399262b651170d6
Chochain Lee <chochain@yahoo.com>
2025-09-10 10:28:33 +0800

    ~/java/com.demo/Elogo.java pass out to Logo2 (for tracing)

--- app/src/main/java/com.demo/Elogo.java
+++ app/src/main/java/com.demo/Elogo.java
@@ -7,26 +7,16 @@ import android.view.View;
 import com.demo.ui.OutputHandler;
 
 public class Elogo {
-    Logo2         logo;
-    OutputHandler out;
+    Logo2         logo;                                      ///< Logo implementation
+    OutputHandler out;                                       ///< for debugging
     
     public Elogo(View vu, OutputHandler out) {
-        this.out = out;
-        
-        logo = new Logo2(vu.getContext(), out);
-        ///
-        ///> resize Logo panel only after layed out (see View life-cycle)
-        ///
-        vu.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
-            @Override
-            public void onLayoutChange(View v,
-                int l1, int t1, int r1, int b1,               ///< new layout
-                int l0, int t0, int r0, int b0) {             ///< orig layout (0,0,0,0)
-                v.removeOnLayoutChangeListener(this);         ///< once, fixed size
-                out.debug("logo w="+v.getWidth()+" h="+v.getHeight()+"\n");
-                logo.reset(v.getWidth(), v.getHeight());
-            }
-        });
+        this.logo = new Logo2(vu.getContext(), out);
+        this.out  = out;
+    }
+
+    public void init(int w, int h) {                         ///< proxy to Logo2
+        logo.reset(w, h);
     }
 
     public void process(String msg) {
@@ -48,8 +38,6 @@ public class Elogo {
         String v1 = n > 1 ? ops[1] : "0";
         String v2 = n > 2 ? ops[2] : "0";
 
-        out.debug("before " + logo.to_s() + "\n");
         logo.execute(op, v1, v2);
-        out.debug("after  " + logo.to_s() + "\n");
     }
 }
