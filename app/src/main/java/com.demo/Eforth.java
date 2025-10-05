414074c45004680ffbd5cd54881b97cfde62b024
Chochain Lee <chochain@yahoo.com>
2025-10-05 14:52:18 -0700

    ~/java/com.demo/Eforth.java separate therad for long running words (with Looper)

--- app/src/main/java/com.demo/Eforth.java
+++ app/src/main/java/com.demo/Eforth.java
@@ -1,36 +1,61 @@
-// ============================================================================
-// ForthProcessor.java - Handles Forth VM operations
+///
+/// @file
+/// @brief - handles messages to/from Forth VM thread
+///
 package com.demo.eforth;
+
+import java.lang.Thread;
+import android.os.Looper;
+import android.os.Handler;
+import android.os.Message;
 
 import com.demo.ui.OutputHandler;
 
-public class Eforth {
+public class Eforth extends Thread {
+    public static final int MSG_TYPE_STR = 1;
+    
     private final String        name;
     private final OutputHandler out;
-    private final JavaCallback  cb;
+    private final JavaCallback  api;
     
-    private IO io;
-    private VM vm;
+    private Handler hndl;
+    private IO      io;
+    private VM      vm;
     
-    public Eforth(String name, OutputHandler out, JavaCallback cb) {
-        this.name  = name;
-        this.out   = out;
-        this.cb    = cb;
+    public Eforth(String name, OutputHandler out, JavaCallback api) {
+        this.name = name;
+        this.out  = out;
+        this.api  = api;
     }
-    
-    public void init() {
-        io = new IO(name, System.in, out);
-        vm = new VM(io, cb);
+
+    @Override
+    public void run() {
+        io   = new IO(name, System.in, out);
+        vm   = new VM(io, api);
         io.mstat();
-    }
-    
-    public void process(String cmd) {
-        out.log(cmd + "\n");
-        io.rescan(cmd);
         
-        while (io.readline()) {
-            if (!vm.outer()) break;
-        }
+        Looper.prepare();
+        hndl = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what != MSG_TYPE_STR) return;
+                String cmd = (String)msg.obj;
+                out.log(cmd + "\n");
+                io.rescan(cmd);
+                 
+                while (io.readline()) {
+                    if (!vm.outer()) break;
+                }
+            }
+        };
+        Looper.loop();
+    }
+
+    public void process(String cmd) {
+        Message msg = Message.obtain();
+        msg.what = MSG_TYPE_STR;
+        msg.obj  = cmd;
+        hndl.sendMessage(msg);
     }
 }
 
