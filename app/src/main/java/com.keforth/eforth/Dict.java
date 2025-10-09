f1bb2dce27febd4cd37d826ab3229a57962f3231
Chochain Lee <chochain@yahoo.com>
2025-10-09 10:30:27 -0700

    ~/java/com.keforth/eforth/Dict.java refactor memory access macros from VM

--- app/src/main/java/com.keforth/eforth/Dict.java
+++ app/src/main/java/com.keforth/eforth/Dict.java
@@ -15,6 +15,21 @@ final public class Dict extends FV<Code> {
         dict.subList(t, dict.size()).clear();           ///> forget words
     }
     ///
+    ///> memory access "macros"
+    ///
+    int  idx() {                                               ///< current dictionary index
+        return ((tail().pf.size() - 1) << 16) | tail().token;
+    }
+    int  getv(int i_w)        {                                ///< get var at i_w 
+        return get(i_w & 0x7fff).get_var(i_w >> 16);
+    }
+    void setv(int i_w, int n) {                                ///< set var at i_w
+        get(i_w & 0x7fff).set_var(i_w >> 16, n);
+    }
+    String gets(int i_w) {                                     ///< get string stored at i_w
+        return get(i_w & 0x7fff).pf.get(i_w >> 16).str;        ///> return a string
+    }
+    ///
     ///> find - Forth dictionary search 
     ///    @param  str  input string to be search against dictionary words
     ///    @return      Code found; null - if not found
