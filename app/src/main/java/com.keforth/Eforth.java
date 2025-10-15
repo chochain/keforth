0230b106b96f0985176efbb4cf75f3b6169f53b8
Chochain Lee <chochain@yahoo.com>
2025-10-15 15:02:20 -0700

    ~/java/com.keforth/Eforth.java add JNI callback handler; alternate run for JNI

--- app/src/main/java/com.keforth/Eforth.java
+++ app/src/main/java/com.keforth/Eforth.java
@@ -14,9 +14,14 @@ import androidx.annotation.NonNull;
 import com.keforth.ui.OutputHandler;
 import com.keforth.eforth.*;
 
-public class Eforth extends Thread {
-    public static final int MSG_TYPE_STR = 1;
-    
+public class Eforth extends Thread implements JavaCallback {
+    public static final int POST_TGT_MAIN   = 0;
+    public static final int POST_TGT_EFORTH = 1;
+    public static final int MSG_TYPE_STR    = 1;
+    private native void forthInit();
+    private native void processJNI(String cmd);
+    private native void forthTeardown();
+
     private final String        name;
     private final OutputHandler out;
     private final JavaCallback  api;
@@ -29,10 +34,11 @@ public class Eforth extends Thread {
         this.name = name;
         this.out  = out;
         this.api  = api;
+
+        forthInit();
     }
 
-    @Override
-    public void run() {
+    public void run0() {
         io   = new IO(name, System.in, out);
         vm   = new VM(io, api);
         io.mstat();
@@ -54,11 +60,33 @@ public class Eforth extends Thread {
         Looper.loop();
     }
 
+    @Override
+    public void run() {
+        Looper.prepare();
+        hndl = new Handler() {
+            @Override public void handleMessage(@NonNull Message msg) {
+                if (msg.what != MSG_TYPE_STR) return;
+                String cmd = (String)msg.obj;
+                out.log(cmd + "\n");
+                processJNI(cmd);
+            }
+        };
+        Looper.loop();
+    }
+
     public void process(String cmd) {
         Message msg = Message.obtain();
         msg.what = MSG_TYPE_STR;
         msg.obj  = cmd;
         hndl.sendMessage(msg);
+    }
+
+    @Override
+    public void onPost(int tid, String rst) {
+ //       if (tid==Eforth.POST_TGT_EFORTH) {
+        out.print(rst);
+ //       }
+ //       else api.onPost(tid, rst);
     }
 }
 
