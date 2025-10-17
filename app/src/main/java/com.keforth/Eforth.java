088db5202c29e787445c9903494307fadaa7c278
Chochain Lee <chochain@yahoo.com>
2025-10-16 18:50:19 -0700

    ~/Eforth.java add USE_JNI_FORTH flag; combine run0=>run; mv onPost to MainActivity

--- app/src/main/java/com.keforth/Eforth.java
+++ app/src/main/java/com.keforth/Eforth.java
@@ -5,70 +5,62 @@
 package com.keforth;
 
 import java.lang.Thread;
+import java.util.Objects;
+
 import android.os.Looper;
 import android.os.Handler;
 import android.os.Message;
 
 import androidx.annotation.NonNull;
 
-import com.keforth.ui.OutputHandler;
 import com.keforth.eforth.*;
+import com.keforth.ui.OutputHandler;
+import static com.keforth.eforth.JavaCallback.PostType.LOG;
 
 public class Eforth extends Thread implements JavaCallback {
-    public static final int POST_TGT_MAIN   = 0;
-    public static final int POST_TGT_EFORTH = 1;
+    public static final int USE_JNI_FORTH   = 1;
     public static final int MSG_TYPE_STR    = 1;
+
     private native void forthInit();
     private native void processJNI(String cmd);
     private native void forthTeardown();
 
+    private static Handler      hndl;
+    private static IO           io;
+    private static VM           vm;
+
     private final String        name;
     private final OutputHandler out;
     private final JavaCallback  api;
-    
-    private Handler hndl;
-    private IO      io;
-    private VM      vm;
     
     public Eforth(String name, OutputHandler out, JavaCallback api) {
         this.name = name;
         this.out  = out;
         this.api  = api;
 
-        forthInit();
-    }
-
-    public void run0() {
-        io   = new IO(name, System.in, out);
-        vm   = new VM(io, api);
-        io.mstat();
-        
-        Looper.prepare();
-        hndl = new Handler() {
-            @Override
-            public void handleMessage(@NonNull Message msg) {
-                if (msg.what != MSG_TYPE_STR) return;
-                String cmd = (String)msg.obj;
-                out.log(cmd + "\n");
-                io.rescan(cmd);
-                 
-                while (io.readline()) {
-                    if (!vm.outer()) break;
-                }
-            }
-        };
-        Looper.loop();
+        if (USE_JNI_FORTH != 0) forthInit();     /// * call JNI eForth constructor
     }
 
     @Override
     public void run() {
-        Looper.prepare();
-        hndl = new Handler() {
+        if (USE_JNI_FORTH == 0) {
+            io = new IO(name, System.in, out);   /// * create IO handlers
+            vm = new VM(io, api);                /// * create Forth VM instance
+            io.mstat();                          /// * display memory usage
+        }
+        Looper.prepare();                        /// * create thread MessageQueue
+        hndl = new Handler(Objects.requireNonNull(Looper.myLooper())) {
             @Override public void handleMessage(@NonNull Message msg) {
                 if (msg.what != MSG_TYPE_STR) return;
                 String cmd = (String)msg.obj;
-                out.log(cmd + "\n");
-                processJNI(cmd);
+                onPost(LOG, cmd);
+                if (USE_JNI_FORTH == 0) {
+                    io.rescan(cmd);              /// * update input stream
+                    while (io.readline()) {      /// * fetch line-by-line
+                        if (!vm.outer()) break;  /// * call Java Forth outer interpreter
+                    }
+                }
+                else processJNI(cmd);            /// * call JNI Forth outer interpreter
             }
         };
         Looper.loop();
@@ -78,16 +70,11 @@ public class Eforth extends Thread implements JavaCallback {
         Message msg = Message.obtain();
         msg.what = MSG_TYPE_STR;
         msg.obj  = cmd;
-        hndl.sendMessage(msg);
+        hndl.sendMessage(msg);                  /// * send command to MessageQueue
     }
 
     @Override
-    public void onPost(int tid, String rst) {
-        switch (tid) {
-        case Eforth.POST_TGT_EFORTH: out.print(rst);       break;
-        case Eforth.POST_TGT_MAIN:   api.onPost(tid, rst); break;
-        default: out.debug("onPost: unknown tid="+Integer.toString(tid));
-        }
+    public void onPost(PostType tid, String rst) {
+        api.onPost(tid, rst);
     }
 }
-
