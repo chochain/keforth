808eb94cec1a7aab9b88f5fd4ab889849282d0fe
Chochain Lee <chochain@yahoo.com>
2025-10-30 19:12:48 -0700

    ~/cpp/main.cpp add JNI_Onload and android_tick (for native ticker proxy)

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -31,21 +31,44 @@ void forth_include(const char *fn) {
     // to JNI
 }
 
+JavaVM    *gJVM        = nullptr;            ///<
 jobject   gForthObj    = nullptr;            ///< Eforth Activity object
+jmethodID gTimerTickID = nullptr;            ///< onNativeTick()
 jmethodID gForthPostID = nullptr;            ///< onNativeForth(rst)
 
 void android_main(struct android_app *app) {
     sensor_engine_start(app);
 }
 
+void android_tick() {                        ///< timer callback (called by ceforth_task)
+    JNIEnv *env;
+    jint rst = gJVM->AttachCurrentThread(&env, nullptr);
+    if (rst != JNI_OK) {
+        gJVM->DetachCurrentThread();
+        return;
+    }
+
+    env->CallVoidMethod(gForthObj, gTimerTickID);
+
+    gJVM->DetachCurrentThread();
+}
+
 extern "C"
 {
+    JNIEXPORT jint JNICALL
+    JNI_OnLoad(JavaVM *vm, void *reserved) {
+        gJVM = vm;
+        return JNI_VERSION_1_6;
+    }
+    
     JNIEXPORT void JNICALL
     Java_com_keforth_Eforth_jniInit(JNIEnv *env, jobject thiz) {
         if (gForthObj != nullptr) env->DeleteGlobalRef(gForthObj);
+
         gForthObj = env->NewGlobalRef(thiz);
         jclass cb = env->GetObjectClass(gForthObj);
 
+        gTimerTickID = env->GetMethodID(cb, "onNativeTick", "()V");
         gForthPostID = env->GetMethodID(cb, "onNativeForth", "(Ljava/lang/String;)V");
 
         env->DeleteLocalRef(cb);
@@ -60,19 +83,20 @@ extern "C"
 
     JNIEXPORT void JNICALL
     Java_com_keforth_Eforth_jniOuter(JNIEnv *env, jobject thiz, jstring js) {
-        static JNIEnv gEnv;
+        static JNIEnv *gEnv;
         // Convert the Java string to a C-style string
         const char *cmd = env->GetStringUTFChars(js, nullptr);
 
         // Check for null if the string conversion fails (e.g., out of memory)
         if (env==nullptr || cmd==nullptr) return;
 
-        gEnv = env;
+        gEnv = env;                            /// * capture JNI Environment
         forth_vm(cmd, [](int, const char *rst){
             /// send Forth response to Eforth.onNativeForth
             gEnv->CallVoidMethod(
-                        gForthObj, gForthPostID, gEnv->NewStringUTF(rst));
+                gForthObj, gForthPostID, gEnv->NewStringUTF(rst));
         });
-        env->ReleaseStringUTFChars(js, cmd);           /// release js, cmd
+        
+        env->ReleaseStringUTFChars(js, cmd);   /// release js, cmd
     }
 }
