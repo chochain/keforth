6afb359294d91bd4f0599d1ac834c8d1deb3c26f
Chochain Lee <chochain@yahoo.com>
2025-11-04 17:49:49 -0800

    ~/java/com.keforth/JavaCallback.java package to com.keforth

---
+++ app/src/main/java/com.keforth/JavaCallback.java
@@ -0,0 +1,12 @@
+///
+/// @file 
+/// @brief - Virtual Machine class
+///
+package com.keforth;
+
+public interface JavaCallback {
+    public enum PostType {
+        LOG, DEBUG, PRINT, FORTH, JAVA
+    }
+    void onPost(PostType tid, String msg);
+}
